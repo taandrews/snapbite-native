a28299a3ec72007cd99b828c0612a00dea2958ad
taandrews <travisandrews@Traviss-MacBook-Pro.local>
2025-08-01 18:17:38 -0400

    Fix Reac Native CLI configuration for IOS build

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  dependencies: {
-    'react-native-vector-icons': {
-      platforms: {
-        ios: {
-          xcodeprojPath: 'ios/SnapBite.xcodeproj',
-          plistPath: 'ios/SnapBite/Info.plist',
-        },
-      },
-    },
-  },
   assets: ['./assets/fonts/'],
 };
